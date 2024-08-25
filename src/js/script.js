72a7ab428a0fcee2a3464c98617d689a27025c56
muzykalniy <andreimuzyka18@gmail.com>
2024-08-25 12:02:57 +0200

    commit 3

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-// Четное или нечетное число
-// Напиши функцию, которая принимает число и выводит, является ли оно четным или нечетным.
+/* Четное или нечетное число
+ Напиши функцию, которая принимает число и выводит, является ли оно четным или нечетным. */
 
 const numInput1 = +prompt('Enter a number:');
 
@@ -15,9 +15,9 @@ const numberType1 = function (num1) {
 const result1 = numberType1(numInput1);
 console.log(result1);
 
-// Положительное, отрицательное или ноль
-// Напиши функцию, которая принимает число и определяет, является ли оно положительным,
-// отрицательным или равным нулю.
+/* Положительное, отрицательное или ноль
+ Напиши функцию, которая принимает число и определяет, является ли оно положительным,
+ отрицательным или равным нулю */
 
 const numInput2 = +prompt('Enter a number:');
 
@@ -28,3 +28,17 @@ const numberType2 = function (num2) {
 };
 const result2 = numberType2(numInput2);
 console.log(result2);
+
+/* Максимум из двух чисел
+Напиши функцию, которая принимает два числа и возвращает большее из них. */
+
+const numInput3 = +prompt('Enter a first number:');
+const numInput4 = +prompt('Enter a second number:');
+
+const numberBig = function (num3, num4) {
+  if (num3 > num4) return `Number ${num3} is bigger`;
+  if (num3 < num4) return `Number ${num4} is bigger`;
+  return 'Both numbers are equal';
+};
+const result3 = numberBig(numInput3, numInput4);
+console.log(result3);
