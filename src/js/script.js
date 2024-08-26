68321717712cfccb8803722baad2082405dad070
muzykalniy <andreimuzyka18@gmail.com>
2024-08-26 19:34:11 +0200

    commit 7

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,4 @@
-/* Четное или нечетное число
- Напиши функцию, которая принимает число и выводит, является ли оно четным или нечетным. */
+/* 1 */
 
 const numInput1 = +prompt('Enter a number:');
 
@@ -12,9 +11,7 @@ const numberType1 = function (num1) {
 const result1 = numberType1(numInput1);
 console.log(result1);
 
-/* Положительное, отрицательное или ноль
- Напиши функцию, которая принимает число и определяет, является ли оно положительным,
- отрицательным или равным нулю */
+/* 2 */
 
 const numInput2 = +prompt('Enter a number:');
 
@@ -26,8 +23,7 @@ const numberType2 = function (num2) {
 const result2 = numberType2(numInput2);
 console.log(result2);
 
-/* Максимум из двух чисел
-Напиши функцию, которая принимает два числа и возвращает большее из них. */
+/* 3 */
 
 const numInput3 = +prompt('Enter a first number:');
 const numInput4 = +prompt('Enter a second number:');
@@ -40,13 +36,7 @@ const numberBig = function (num3, num4) {
 const result3 = numberBig(numInput3, numInput4);
 console.log(result3);
 
-/* Оценка по баллам
-Напиши программу, которая принимает на вход число (от 0 до 100) и
-выводит оценку по следующим критериям:
-90-100: Отлично
-75-89: Хорошо
-50-74: Удовлетворительно
-0-49: Неудовлетворительно */
+/* 4 */
 
 const numInput5 = prompt('Enter a number from 0 to 100:'); // Не приводим к числу сразу
 
@@ -67,9 +57,7 @@ const note = function (num5) {
 const result4 = note(numInput5);
 console.log(result4);
 
-/* Простой калькулятор
-Напиши программу, которая принимает два числа и оператор (+, -, *, /),
-а затем возвращает результат выполнения этого оператора над этими числами. */
+/* 5 */
 const numInput6 = prompt('Enter a first number:');
 const numInput7 = prompt('Enter a second number:');
 const operatorInput = prompt('Enter an operator:');
@@ -94,8 +82,75 @@ const calculator = function (num6, num7) {
       }
       return num6 / num7;
     }
+    if (operatorInput === '=') {
+      return 'You should enter action operator';
+    }
   }
   return 'error';
 };
 const result5 = calculator(numInput6, numInput7);
 console.log(result5);
+
+/* 6 */
+
+const xInput = prompt('Enter a X axes:');
+const yInput = prompt('Enter a Y axes:');
+
+const axesFunction = function (x, y) {
+  if (x !== null && x.trim() !== '' && !isNaN(x)
+&& y !== null && y.trim() !== '' && !isNaN(y)) {
+    x = Number(x);
+    y = Number(y);
+    if (x === 0 && y === 0) return 'it is a center';
+    if (x > 0 && y > 0) return 'it is 1 quarter';
+    if (x < 0 && y > 0) return 'it is 2 quarter';
+    if (x < 0 && y < 0) return 'it is 3 quarter';
+    if (x > 0 && y < 0) return 'it is 4 quarter';
+    if (x === 0 || y === 0) return 'the point is on the axe';
+  }
+  return 'error';
+};
+const result6 = axesFunction(xInput, yInput);
+console.log(result6);
+
+/* 7 */
+
+const aInput = prompt('Enter first side length:');
+const bInput = prompt('Enter second side length:');
+const cInput = prompt('Enter third side length:');
+
+const aInputNum = Number(aInput);
+const bInputNum = Number(bInput);
+const cInputNum = Number(cInput);
+
+const triangle = function (a, b, c) {
+  if (!isNaN(a) && !isNaN(b) && !isNaN(c)) {
+    if (a <= 0 || b <= 0 || c <= 0) return "This triangle doesn't exist";
+
+    if (a === b && b === c) return 'This is an equilateral triangle';
+    if (a === b || b === c || a === c) return 'This is an isosceles triangle';
+    return 'This is a scalene triangle';
+  }
+  return 'Error: invalid input';
+};
+const result7 = triangle(aInputNum, bInputNum, cInputNum);
+console.log(result7);
+
+/* 8 */
+
+const yearInput = prompt('Enter year:');
+const yearNum = Number(yearInput);
+
+const leapYear = function (year) {
+  if (!isNaN(year)) {
+    if ((year % 4 === 0 || year % 100 !== 0) && year % 400 === 0) {
+      return 'It is a leap year';
+    }
+    return 'It is not a leap year';
+  }
+  return 'Error: invalid input';
+};
+const result8 = leapYear(yearNum);
+console.log(result8);
+
+/* 9 */
