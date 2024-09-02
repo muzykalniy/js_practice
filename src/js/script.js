de88610b0f760ec1e5470fdb80d1cdb827e49545
muzykalniy <andreimuzyka18@gmail.com>
2024-09-03 00:24:34 +0200

    commit 3

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,16 @@
 'use strict';
 
 
+// 1
+
+const doSomething = function (number, callback) {
+  const result = number * 2;
+  callback(result);
+};
+doSomething(5, (result) => {
+  console.log('Result of ', result);
+});
+
+// 2
+
+
