01e723a09af0f1a06c5031c077ca6dbf46707167
muzykalniy <andreimuzyka18@gmail.com>
2024-09-03 17:42:57 +0200

    commit 4

--- src/js/script.js
+++ src/js/script.js
@@ -12,3 +12,44 @@ doSomething(5, (result) => {
 });
 
 // 2
+const processNumber = function (number, callback) {
+// Проверь четность числа и вызови callback, если число четное
+  if (number % 2 === 0) {
+    callback(number);
+  } else callback(number);
+};
+
+processNumber(4, (number) => {
+  console.log('Четное число:', number);
+});
+processNumber(3, (number) => {
+  console.log('Нечетное число:', number);
+});
+
+// 3
+const processArray = function (arr, callback) {
+// Пройди по каждому элементу массива и вызови callback
+  for (const item of arr) {
+    const itemDouble = item * 2;
+    callback(itemDouble);
+  }
+};
+
+processArray([1, 2, 3], (itemDouble) => {
+  console.log('Обработанный элемент:', itemDouble);
+});
+
+// 4
+function filterArray(arr, callback) {
+// Создай новый массив, добавляя элементы, которые проходят проверку callback
+  const newArr = [];
+  for (const item of arr) {
+    if (callback(item)) {
+      newArr.push(item);
+    }
+  }
+  return newArr;
+}
+
+const result = filterArray([1, 2, 3, 4, 5], (item) => item > 3);
+console.log(result); // Должно вывести: [4, 5]
