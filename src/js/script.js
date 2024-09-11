f8e60e58f912b9f14209eb6fe2ea337aea16f707
muzykalniy <andreimuzyka18@gmail.com>
2024-09-11 13:34:46 +0200

    commit 5

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,14 @@
 'use strict';
 
+// 1
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const res1 = [...array1, ...array2];
+console.log(res1);
+
+// 2
+const res2 = array1.reverse();
+console.log(res2);
+
+// 3
 
